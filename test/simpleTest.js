36f8a263991b300731a16ee81d316b9966181e0f
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2016-06-09 17:02:44 +0200

    Updated test script.

--- test/simpleTest.js
+++ test/simpleTest.js
@@ -10,9 +10,23 @@ probe.getSerialNumbers((err, serialNumbers) => {
     console.log(nrfjprog.NRF52_FAMILY);
 
     probe.program(serialNumbers[0].serialNumber, { 0: 'connectivity_115k2_with_s130_2.0.1.hex', 1: 'connectivity_115k2_with_s132_2.0.1.hex'}, err => {
-        console.log(err);
+        console.log('Error: ' + err);
         console.log('Done programming');
-    });
 
-    probe.getVersion(serialNumbers[0].serialNumber, (err, version) => { console.log('Error: ' + err); console.log('Version: ' + version);});
+        probe.getVersion(serialNumbers[0].serialNumber, (err, version) => {
+            console.log('Error: ' + err);
+            console.log('Read version 1: ' + version);
+
+            probe.getVersion(serialNumbers[0].serialNumber, (err, version2) => {
+                console.log('Error: ' + err);
+                console.log('Read version 2: ' + version2);
+
+                if (version === version2) {
+                    console.log('Version read correctly.');
+                } else {
+                    console.log('Version read wrongly.');
+                }
+            });
+        });
+    });
 });
