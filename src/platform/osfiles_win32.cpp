92b43f066afb11deb5a409105d537b61f6f63885
Per Kristian Schanke <CoqRogue@users.noreply.github.com>
2017-04-04 13:43:23 +0200

    Revert "Deleted duplicated code and fixed compile warnings."

--- src/platform/osfiles_win32.cpp
+++ src/platform/osfiles_win32.cpp
@@ -1,6 +1,5 @@
-#ifndef _CRT_SECURE_NO_WARNINGS
+
 #define _CRT_SECURE_NO_WARNINGS
-#endif
 
 #include "Shlwapi.h"
 #include <windows.h>
@@ -22,7 +21,7 @@ bool OSFilesExists(char * path)
 #define MAX_KEY_LENGTH 1000
 #define MAX_VALUE_NAME 1000
 
-NrfjprogErrorCodesType OSFilesFindDllByHKey(const HKEY rootKey, char * dll_path, int dll_path_len)
+NrfjprogErrorCodesType OSFilesFindDll(char * dll_path, int dll_path_len)
 {
     HKEY key;
     HKEY innerKey;
@@ -31,7 +30,7 @@ NrfjprogErrorCodesType OSFilesFindDllByHKey(const HKEY rootKey, char * dll_path,
     DWORD install_path_size = sizeof(install_path);
 
     /* Search for JLinkARM in the Local Machine Key.  */
-    if (RegOpenKeyEx(rootKey, "Software\\Nordic Semiconductor\\nrfjprog", 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS, &key) == ERROR_SUCCESS)
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "Software\\Nordic Semiconductor\\nrfjprog", 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS, &key) == ERROR_SUCCESS)
     {
         TCHAR    achKey[MAX_KEY_LENGTH];   // buffer for subkey name
         DWORD    cbName;                   // size of name string
@@ -48,6 +47,7 @@ NrfjprogErrorCodesType OSFilesFindDllByHKey(const HKEY rootKey, char * dll_path,
 
         DWORD retCode;
 
+        TCHAR  achValue[MAX_VALUE_NAME];
         DWORD cchValue = MAX_VALUE_NAME;
 
         retCode = RegQueryInfoKey(
@@ -100,19 +100,76 @@ NrfjprogErrorCodesType OSFilesFindDllByHKey(const HKEY rootKey, char * dll_path,
     }
 
     /* Search for JLinkARM in the Current User Key.  */
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Nordic Semiconductor\\nrfjprog", 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS, &key) == ERROR_SUCCESS)
+    {
+        TCHAR    achKey[MAX_KEY_LENGTH];   // buffer for subkey name
+        DWORD    cbName;                   // size of name string
+        TCHAR    achClass[MAX_PATH] = TEXT("");  // buffer for class name
+        DWORD    cchClassName = MAX_PATH;  // size of class string
+        DWORD    cSubKeys = 0;               // number of subkeys
+        DWORD    cbMaxSubKey;              // longest subkey size
+        DWORD    cchMaxClass;              // longest class string
+        DWORD    cValues;              // number of values for key
+        DWORD    cchMaxValue;          // longest value name
+        DWORD    cbMaxValueData;       // longest value data
+        DWORD    cbSecurityDescriptor; // size of security descriptor
+        FILETIME ftLastWriteTime;      // last write time
 
-    return NrfjprogDllNotFoundError;
-}
+        DWORD retCode;
 
-NrfjprogErrorCodesType OSFilesFindDll(char * dll_path, int dll_path_len)
-{
-    NrfjprogErrorCodesType retCode = OSFilesFindDllByHKey(HKEY_LOCAL_MACHINE, dll_path, dll_path_len);
+        TCHAR  achValue[MAX_VALUE_NAME];
+        DWORD cchValue = MAX_VALUE_NAME;
 
-    if (retCode != Success) {
-        retCode = OSFilesFindDllByHKey(HKEY_CURRENT_USER, dll_path, dll_path_len);
+        retCode = RegQueryInfoKey(
+            key,                    // key handle
+            achClass,                // buffer for class name
+            &cchClassName,           // size of class string
+            NULL,                    // reserved
+            &cSubKeys,               // number of subkeys
+            &cbMaxSubKey,            // longest subkey size
+            &cchMaxClass,            // longest class string
+            &cValues,                // number of values for this key
+            &cchMaxValue,            // longest value name
+            &cbMaxValueData,         // longest value data
+            &cbSecurityDescriptor,   // security descriptor
+            &ftLastWriteTime);       // last write time
+
+        // Enumerate the subkeys, until RegEnumKeyEx fails.
+
+        if (cSubKeys)
+        {
+            cbName = MAX_KEY_LENGTH;
+            retCode = RegEnumKeyEx(key,
+                                   cSubKeys - 1,
+                                   achKey,
+                                   &cbName,
+                                   NULL,
+                                   NULL,
+                                   NULL,
+                                   &ftLastWriteTime);
+            RegOpenKeyEx(key, achKey, 0, KEY_QUERY_VALUE, &innerKey);
+        }
+
+        /* If it is found, read the install path. */
+        if (RegQueryValueEx(innerKey, "InstallPath", NULL, NULL, (LPBYTE)&install_path, &install_path_size) == ERROR_SUCCESS)
+        {
+            /* Copy, check it exists and return if it does. */
+            strncpy(dll_path, install_path, dll_path_len);
+            strncat(dll_path, "nrfjprog.dll", dll_path_len - strlen(dll_path) - 1);
+            RegCloseKey(innerKey);
+            RegCloseKey(key);
+            if (OSFilesExists(dll_path))
+            {
+                return Success;
+            }
+        }
+
+        /* In case we did not obtain the path, for whatever reason, we close the key. */
+        RegCloseKey(innerKey);
+        RegCloseKey(key);
     }
 
-    return retCode;
+    return NrfjprogDllNotFoundError;
 }
 
 NrfjprogErrorCodesType OSFilesFindJLink(char * jlink_path, int jlink_path_len)
