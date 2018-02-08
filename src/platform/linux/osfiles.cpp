bcd45d803540da906a23baa845288698ff0694bd
Iván Sánchez Ortega <ivan@sanchezortega.es>
2018-02-08 17:01:13 +0100

    WIP: Preliminary work to load the high-level DLLs from a path relative to the compiled node module

--- src/platform/linux/osfiles.cpp
+++ src/platform/linux/osfiles.cpp
@@ -37,7 +37,7 @@
 #include "../../osfiles.h"
 
 #include <sys/stat.h>
-#include <string.h>
+#include <string>
 #include <libgen.h>
 #include <dlfcn.h>
 
@@ -47,13 +47,24 @@
 #include <unistd.h>
 
 #include <iostream>
+#include <fstream>
+// #include <sstream>
+// #include <regex>
+#include <dirent.h>
 
+/* Try to locate a dynamically-linked library named fileName, and set dll_path
+ * to the full path to that library.
+ */
 errorcode_t OSFilesFindDll(std::string &dll_path, std::string &fileName)
 {
     char temp_dll_path[COMMON_MAX_PATH];
     memset(temp_dll_path, 0, COMMON_MAX_PATH);
 
-    ssize_t len = readlink("/proc/self/exe", temp_dll_path, COMMON_MAX_PATH - 1);
+    // Fetch path of currently running executable
+    ssize_t len;
+    len = readlink("/proc/self/exe", temp_dll_path, COMMON_MAX_PATH - 1);
+
+    printf("\n/proc/self/exe points to: %s\n\n", temp_dll_path);
 
     if (len == -1)
     {
@@ -64,22 +75,95 @@ errorcode_t OSFilesFindDll(std::string &dll_path, std::string &fileName)
     dll_path.append("/");
     dll_path.append(fileName);
 
-    if (!AbstractFile::pathExists(dll_path))
+    printf("\nTrying to load %s from: %s\n\n", fileName.c_str(), dll_path.c_str());
+
+    // If there is a file with the requested fileName in the same path as the
+    // current node.js (or electron) executable, use that.
+    if (AbstractFile::pathExists(dll_path))
     {
-        /* It is possible that the user might have place the .dylib in another folder. In that case dlopen will find it. If it is not found, return JLinkARMDllNotFoundError. */
-        void * libraryHandle = dlopen(fileName.c_str(), RTLD_LAZY);
-
-        if (libraryHandle)
-        {
-            dlclose(libraryHandle);
-            dll_path = fileName;
-            return errorcode_t::JsSuccess;
-        }
-
-        return errorcode_t::CouldNotFindJprogDLL;
+        return errorcode_t::JsSuccess;
     }
 
-    return errorcode_t::JsSuccess;
+    // Try fetching paths of all loaded libraries by resolving symlinks from
+    // all entries at /proc/self/map_files
+
+    DIR *dir;
+    struct dirent *entry;
+    char mapped_file_name[COMMON_MAX_PATH];
+    char mem_region_file_name[COMMON_MAX_PATH];
+    dir = opendir("/proc/self/map_files");
+
+    while((entry = readdir(dir)) != NULL) {
+        memset(mapped_file_name, 0, COMMON_MAX_PATH);
+        memset(mem_region_file_name, 0, COMMON_MAX_PATH);
+        strcpy(mem_region_file_name, "/proc/self/map_files/");
+        strcat(mem_region_file_name, entry->d_name);
+        len = readlink(mem_region_file_name, mapped_file_name, COMMON_MAX_PATH - 1);
+        if (len != -1) {
+            printf("%s → %s\n", entry->d_name, mapped_file_name );
+            dll_path.assign(dirname(mapped_file_name));
+            dll_path.append("/");
+            dll_path.append(fileName);
+
+            printf("\nTrying to load %s from: %s\n", fileName.c_str(), dll_path.c_str());
+            if (AbstractFile::pathExists(dll_path))
+            {
+                printf("Success!!\n");
+                return errorcode_t::JsSuccess;
+            } else {
+                printf("Nope :-(\n\n");
+            }
+
+        } else {
+            printf("%s %d\n", entry->d_name, errno);
+        }
+
+    }
+
+
+
+
+
+    // Try fetching paths of all loaded libraries by reading /proc/self/maps and
+    // parsing its contents, look for loaded *.node files
+//     std::ifstream ifs("/proc/self/maps");
+// //     std::string maps( (std::istreambuf_iterator<char>(ifs) ),
+// //                          (std::istreambuf_iterator<char>()    ) );
+//
+//     printf("\n/proc/self/maps contains: \n\n");
+//
+//     // Use ECMAScript-style regexps because of \S character class
+// //     std::regex nodeRegExp("(\\S+\\.node)$");
+// //     std::regex nodeRegExp("(\\S*)/build/Release/pc-nrfjprog-js\\.node$");
+//     std::regex nodeRegExp("(.*)/build/Release/pc-nrfjprog-js\\.node$");
+// //     std::regex nodeRegExp("(.+\\.node)$");
+//     std::smatch nodeRegExpMatch;
+//
+//     for (std::string line; std::getline(ifs, line); ) {
+//         printf("%s\n", line.c_str());
+//         if (std::regex_match(line, nodeRegExpMatch, nodeRegExp)) {
+//             printf("Found!!!!!!1!\n");
+//             printf(">> %s <<\n", nodeRegExpMatch.str().c_str());
+//         }
+//
+// //         cout << to <<endl;
+//     }
+
+    // Last recourse, try loading the library through dlopen().
+    // That will look into /usr/lib and into whatever LD_LIBRARY_PATH looks into.
+    printf("\nTrying to load %s through dlopen()\n\n", fileName.c_str() );
+
+    void * libraryHandle = dlopen(fileName.c_str(), RTLD_LAZY);
+
+    if (libraryHandle)
+    {
+        dlclose(libraryHandle);
+        dll_path = fileName;
+        return errorcode_t::JsSuccess;
+    }
+
+    // If the library hasn't been found, return JLinkARMDllNotFoundError
+    return errorcode_t::CouldNotFindJprogDLL;
 }
 
 std::string TempFile::concatPaths(std::string base_path, std::string relative_path)
