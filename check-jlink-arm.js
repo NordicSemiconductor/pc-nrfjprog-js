441c4446f88fea03ab9be5e67f02abd805775b5f
Magne Rodem <magne@rodem.no>
2018-02-20 12:35:47 +0100

    Check for both J-Link and nrfjprog libs after install

--- check-jlink-arm.js
+++ check-jlink-arm.js
@@ -1,44 +1,59 @@
-
 // Small script run *after* the node binary module is either fetched or compiled.
 
 /*
- * J-link libraries are required for the nrfjprog libraries, which are required
- * for programming. This script warns the user if these libs are missing and points
- * them to the right place to look for a solution.
+ * Both nrfjprog and J-link libraries are required for pc-nrfjprog-js to function.
+ * This script warns the user if these libs are missing and points them to the
+ * right place to look for a solution.
  */
+
+'use strict';
 
 const chalk = require('chalk');
 
-const libraryVersion = new Promise((res, rej)=>{
-    try {
-        const nrfjprog = require('.');
-        nrfjprog.getLibraryVersion((err, version)=>{
-            if (err) { rej(err) } else {res(version)}
-        })
-    } catch(ex) {
-        rej(ex);
-    }
-});
+function getLibraryVersion() {
+    return new Promise((resolve, reject) => {
+        try {
+            // eslint-disable-next-line global-require
+            const nrfjprog = require('.');
+            nrfjprog.getLibraryVersion((err, version) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(version);
+                }
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+}
 
+getLibraryVersion()
+    .then(version => {
+        console.log('nrfjprog libraries are at version', version);
+        console.log('J-Link libraries seem to be working as expected.');
+    })
+    .catch(err => {
+        console.log();
+        console.log(chalk.red.bold(`Found error: ${err.errcode}`));
 
-libraryVersion
-.then((version)=>{
-    console.log('nrfjprog libraries at version ', version, ', seems that J-Link libraries are working as expected');
-})
-.catch((err)=>{
-    if (err.errno == 7 &&
-        err.errcode === "CouldNotOpenDLL" &&
-        err.lowlevelError === "JLINKARM_DLL_NOT_FOUND") {
+        const isNrfjprogError = err.errno === 2 && err.errcode === 'CouldNotFindJprogDLL';
+        const isJlinkError = err.errno === 7 && err.errcode === 'CouldNotOpenDLL'
+            && err.lowlevelError === 'JLINKARM_DLL_NOT_FOUND';
+
+        if (isNrfjprogError) {
+            console.log('WARNING: The automated setup of nrfjprog libraries failed. The pc-nrfjprog-js');
+            console.log('library cannot function without them.');
+            console.log(chalk.bold('Please install nRF5x Command Line Tools manually from the Nordic'));
+            console.log(chalk.bold('Semiconductor website.'));
+        } else if (isJlinkError) {
+            console.log('WARNING: The J-Link libraries were not found on your system. The pc-nrfjprog-js');
+            console.log('library cannot function without them.');
+            console.log(chalk.bold('Please visit https://www.segger.com/downloads/jlink/ and install'));
+            console.log(chalk.bold('the "J-Link Software and Documentation Pack".'));
+        } else {
+            console.log('The pc-nrfjprog-js post-install check failed unexpectedly:', err.message);
+        }
 
         console.log();
-        console.log(chalk.red.bold("Found error: JLINKARM_DLL_NOT_FOUND"));
-        console.log("WARNING: The j-link libraries were not found on your system,");
-        console.log("nrfjprog-js cannot function without them.");
-        console.log(chalk.bold("Please visit https://www.segger.com/downloads/jlink/ and"));
-        console.log(chalk.bold("install the \"J-Link Software and Documentation Pack\"."));
-        console.log();
-    }
-     return Promise.reject(err);
-
-});
-
+    });
