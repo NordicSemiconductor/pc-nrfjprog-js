7ed6261aafe29543279f2dab717946fd6764d178
Per Kristian Schanke <per.kristian.schanke@nordicsemi.no>
2019-01-28 12:45:18 +0100

    Fix to build correctly on 64 bit Windows.

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -9,9 +9,9 @@ add_definitions(
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4200")
 
 # Env variable hack as per http://cmake.3232098.n2.nabble.com/CMP0053-Unable-to-refer-to-ENV-PROGRAMFILES-X86-td7588994.html
-set(PROGRAMFILESX86 "PROGRAMFILES(X86)") 
+set(PROGRAMFILESX86 "PROGRAMFILES(X86)")
 
-set(PLATFORM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src/platform/win $ENV{${PROGRAMFILESX86}}/Nordic\ Semiconductor/nrf5x/bin/headers $ENV{PROGRAMFILES}/Nordic\ Semiconductor/nrf5x/bin/headers)
+set(PLATFORM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src/platform/win $ENV{${PROGRAMFILESX86}}/Nordic\ Semiconductor/nrf5x/bin/headers $ENV{ProgramW6432}/Nordic\ Semiconductor/nrf5x/bin/headers)
 
 file (GLOB PLATFORM_SOURCE_FILES
     "src/platform/win/*.cpp"
